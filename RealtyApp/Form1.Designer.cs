c5730c225aff6a1d17985ee748a0114bb2457365
Dusty Dean Account <dustinpdean@yahoo.com>
2021-04-03 22:25:14 -0500

    Add project files.

---
+++ RealtyApp/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace RealtyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtAddress1 = new System.Windows.Forms.TextBox();
+            this.txtAddress2 = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.rtbNotes = new System.Windows.Forms.RichTextBox();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.lblAddress1 = new System.Windows.Forms.Label();
+            this.lblAddress2 = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblZip = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.livHouses = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Permanent Marker", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 292);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(272, 210);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "This is where a map would go if DotNetBrowser allowed for a free trial greater th" +
+    "an 30 days.";
+            // 
+            // txtAddress1
+            // 
+            this.txtAddress1.Enabled = false;
+            this.txtAddress1.Location = new System.Drawing.Point(12, 41);
+            this.txtAddress1.Name = "txtAddress1";
+            this.txtAddress1.Size = new System.Drawing.Size(272, 23);
+            this.txtAddress1.TabIndex = 1;
+            // 
+            // txtAddress2
+            // 
+            this.txtAddress2.Enabled = false;
+            this.txtAddress2.Location = new System.Drawing.Point(12, 86);
+            this.txtAddress2.Name = "txtAddress2";
+            this.txtAddress2.Size = new System.Drawing.Size(272, 23);
+            this.txtAddress2.TabIndex = 2;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Enabled = false;
+            this.txtCity.Location = new System.Drawing.Point(12, 131);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(167, 23);
+            this.txtCity.TabIndex = 3;
+            // 
+            // txtState
+            // 
+            this.txtState.Enabled = false;
+            this.txtState.Location = new System.Drawing.Point(185, 131);
+            this.txtState.MaxLength = 2;
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(27, 23);
+            this.txtState.TabIndex = 4;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Enabled = false;
+            this.txtZipCode.Location = new System.Drawing.Point(218, 131);
+            this.txtZipCode.MaxLength = 10;
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(66, 23);
+            this.txtZipCode.TabIndex = 5;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Enabled = false;
+            this.txtStatus.Location = new System.Drawing.Point(12, 178);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(272, 23);
+            this.txtStatus.TabIndex = 6;
+            // 
+            // rtbNotes
+            // 
+            this.rtbNotes.Enabled = false;
+            this.rtbNotes.Location = new System.Drawing.Point(290, 310);
+            this.rtbNotes.MaxLength = 500;
+            this.rtbNotes.Name = "rtbNotes";
+            this.rtbNotes.Size = new System.Drawing.Size(236, 192);
+            this.rtbNotes.TabIndex = 7;
+            this.rtbNotes.Text = "";
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Enabled = false;
+            this.txtRegion.Location = new System.Drawing.Point(12, 222);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(272, 23);
+            this.txtRegion.TabIndex = 8;
+            // 
+            // lblAddress1
+            // 
+            this.lblAddress1.AutoSize = true;
+            this.lblAddress1.Location = new System.Drawing.Point(12, 23);
+            this.lblAddress1.Name = "lblAddress1";
+            this.lblAddress1.Size = new System.Drawing.Size(58, 15);
+            this.lblAddress1.TabIndex = 9;
+            this.lblAddress1.Text = "Address 1";
+            // 
+            // lblAddress2
+            // 
+            this.lblAddress2.AutoSize = true;
+            this.lblAddress2.Location = new System.Drawing.Point(12, 68);
+            this.lblAddress2.Name = "lblAddress2";
+            this.lblAddress2.Size = new System.Drawing.Size(58, 15);
+            this.lblAddress2.TabIndex = 10;
+            this.lblAddress2.Text = "Address 2";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(12, 113);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(28, 15);
+            this.lblCity.TabIndex = 11;
+            this.lblCity.Text = "City";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(185, 113);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(33, 15);
+            this.lblState.TabIndex = 12;
+            this.lblState.Text = "State";
+            // 
+            // lblZip
+            // 
+            this.lblZip.AutoSize = true;
+            this.lblZip.Location = new System.Drawing.Point(218, 113);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(55, 15);
+            this.lblZip.TabIndex = 13;
+            this.lblZip.Text = "Zip Code";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 160);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 15);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(12, 204);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(44, 15);
+            this.lblRegion.TabIndex = 15;
+            this.lblRegion.Text = "Region";
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Location = new System.Drawing.Point(290, 292);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(38, 15);
+            this.lblNotes.TabIndex = 16;
+            this.lblNotes.Text = "Notes";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(12, 251);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(88, 35);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(206, 12);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(35, 23);
+            this.btnPrevious.TabIndex = 19;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(247, 12);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(35, 23);
+            this.btnNext.TabIndex = 20;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // livHouses
+            // 
+            this.livHouses.HideSelection = false;
+            this.livHouses.Location = new System.Drawing.Point(532, 12);
+            this.livHouses.Name = "livHouses";
+            this.livHouses.Size = new System.Drawing.Size(251, 490);
+            this.livHouses.TabIndex = 21;
+            this.livHouses.UseCompatibleStateImageBehavior = false;
+            this.livHouses.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 511);
+            this.Controls.Add(this.livHouses);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lblNotes);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblZip);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress2);
+            this.Controls.Add(this.lblAddress1);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.rtbNotes);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress2);
+            this.Controls.Add(this.txtAddress1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Dean Realty Helper that Helps Realtors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtAddress1;
+        private System.Windows.Forms.TextBox txtAddress2;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.RichTextBox rtbNotes;
+        private System.Windows.Forms.TextBox txtRegion;
+        private System.Windows.Forms.Label lblAddress1;
+        private System.Windows.Forms.Label lblAddress2;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblZip;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblRegion;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ListView livHouses;
+    }
+}
+
